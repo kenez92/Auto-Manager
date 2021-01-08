b011f670ac8c3f4792fd715081b5b53189996c2d
Daniel <daniel.kaminski@smartive.app>
2021-01-08 12:02:20 +0100

    Initial commit

---
+++ src/test/java/pl/car/automanager/AutoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.car.automanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
